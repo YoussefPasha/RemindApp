b88184a850ad802227d053e3a52b77fe3f6769b6
youssefpasha <youssefbashafcis@gmail.com>
2023-10-17 05:09:43 +0300

    collection form and server actions

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,19 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
-import { wait } from "@/lib/wait";
+import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs";
 import { Suspense } from "react";
+import SadFace from "@/components/icons/SadFace";
+import CreateCollectionBtn from "./_components/CreateCollectionBtn";
 
 export default async function Home() {
   return (
     <>
       <Suspense fallback={<WelcomeMsgFallback />}>
         <WelcomeMessage />
+      </Suspense>
+      <Suspense fallback={<CollectionWaitFallback />}>
+        <CollectionList />
       </Suspense>
     </>
   );
@@ -35,9 +41,58 @@ function WelcomeMsgFallback() {
   return (
     <div className="flex w-full mb-12">
       <h1 className="text-4xl font-bold">
-        <Skeleton className="w-[180px] h-[36px]" />
+        <Skeleton className="w-[180px] h-[36px] mb-10 " />
         <Skeleton className="w-[150px] h-[36px]" />
       </h1>
     </div>
+  );
+}
+
+function CollectionWaitFallback() {
+  return (
+    <div className="flex w-full mb-12">
+      <h1 className="text-4xl font-bold">
+        <Skeleton className="w-[500px] h-[100px] mb-10" />
+        <Skeleton className="w-[600px] h-[100px]" />
+      </h1>
+    </div>
+  );
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    include: {
+      tasks: true,
+    },
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  if (collections.length === 0) {
+    return (
+      <div className="flex flex-col gap-5">
+        <Alert>
+          <SadFace />
+          <AlertTitle>There are no collections yet!</AlertTitle>
+          <AlertDescription>
+            Create a collection to get started
+          </AlertDescription>
+        </Alert>
+        <CreateCollectionBtn />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <CreateCollectionBtn />
+      {/* <div className="flex flex-col gap-4 mt-6">
+        {collections.map((collection) => (
+          <CollectionCard key={collection.id} collection={collection} />
+        ))}
+      </div> */}
+    </>
   );
 }
